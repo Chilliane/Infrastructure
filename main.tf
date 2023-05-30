d5730166e2d100c43b7188b0f06fef8f1f740bcd
Basil Guevarra <basilioguevarra@gmail.com>
2023-05-30 22:22:01 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -25,15 +25,52 @@ resource "aws_vpc" "example" {
 variable "subnets_cidr" {
 	type = list
 	default = ["10.0.1.0/24", "10.0.2.0/24"]
+  #10.0.1.0/24 will be public , 10.0.2.0/24 will be private
+}
+#Variable name for the subnets
+variable "subnet_names"{
+  type = list 
+  default = ["Public","Private"]
 }
 
+
 #Create Public Subnet
-resource "aws_subnet" "public" {
+resource "aws_subnet" "subnet" {
   count = length(var.subnets_cidr)
   vpc_id = aws_vpc.example.id
   cidr_block = element(var.subnets_cidr,count.index)
   map_public_ip_on_launch = true
   tags = {
-    Name = "Subnet-${count.index+1}"
+    Name = "TerraformSubnet-${var.subnet_names[count.index]}"
+  }
+}
+
+#Create Internet Gateways
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.example.id
+
+  tags = {
+    Name = "TerraformInternetGateway"
+  }
+}
+
+#Create public routing table
+resource "aws_route_table" "public_routing_table" {
+  vpc_id = aws_vpc.example.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id #Provide connection to Internet
+  }
+  tags = {
+    Name = "RouteTerraformPublic"
+  }
+}
+#Create private routing table
+resource "aws_route_table" "private_routing_table" {
+  vpc_id = aws_vpc.example.id
+
+  tags = {
+    Name = "RouteTerraformPrivate"
   }
 }
