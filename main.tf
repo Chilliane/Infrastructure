cea6fc924578897048601cab098e1adbed3ade45
Basil Guevarra <basilioguevarra@gmail.com>
2023-05-30 23:12:53 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -57,9 +57,10 @@ resource "aws_internet_gateway" "gw" {
 #Create public routing table
 resource "aws_route_table" "public_routing_table" {
   vpc_id = aws_vpc.example.id
+  #Provide connection to Internet
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.gw.id #Provide connection to Internet
+    gateway_id = aws_internet_gateway.gw.id 
   }
   tags = {
     Name = "RouteTerraformPublic"
@@ -74,7 +75,7 @@ resource "aws_route_table" "private_routing_table" {
 }
 # Create public Routing Table Association
 resource "aws_route_table_association" "public" {
-  subnet_id = aws_subnet.subnet[0].id # Public subnet
+  subnet_id = aws_subnet.subnet[0].id # public subnet
   route_table_id = aws_route_table.public_routing_table.id
 }
 
@@ -82,4 +83,57 @@ resource "aws_route_table_association" "public" {
 resource "aws_route_table_association" "private" {
   subnet_id = aws_subnet.subnet[1].id # private subnet
   route_table_id = aws_route_table.private_routing_table.id
+}
+#Create EC2 Instance
+resource "aws_instance" "publicEC2" {
+  ami = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
+  subnet_id = aws_subnet.subnet[0].id
+  associate_public_ip_address = true
+  security_groups = [aws_security_group.allowssh.id]
+  tags = {
+    Name = "PublicEC2"
+  }
+}
+
+#Ubuntu Image for the EC2 Instance
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+#Securty Policy to Allow SSH
+resource "aws_security_group" "allowssh" {
+  name        = "allow_ssh"
+  description = "Allow SSH inbound traffic"
+  vpc_id      = aws_vpc.example.id
+
+  ingress {
+    description      = "SSH from VPC"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "Allow_SSH"
+  }
 }
